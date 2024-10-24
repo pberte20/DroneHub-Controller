e1e5a4c0f835cc5b68659b93660ea8200811bf7f
mhoang17 <31686412+mhoang17@users.noreply.github.com>
2024-10-24 21:35:13 +0200

    Copied FPVDemo code to quick start the MainActivity code

--- app/src/main/java/com/aau/herd/controller/ConnectionActivity.java
+++ app/src/main/java/com/aau/herd/controller/ConnectionActivity.java
@@ -1,93 +1,192 @@
 package com.aau.herd.controller;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import dji.common.error.DJIError;
 import dji.common.error.DJISDKError;
+import dji.log.DJILog;
+import dji.sdk.base.BaseComponent;
 import dji.sdk.base.BaseProduct;
 import dji.sdk.products.Aircraft;
+import dji.sdk.sdkmanager.DJISDKInitEvent;
 import dji.sdk.sdkmanager.DJISDKManager;
 
-public class ConnectionActivity extends AppCompatActivity implements View.OnClickListener {
+public class ConnectionActivity extends Activity implements View.OnClickListener {
 
-    private static final String TAG = MainActivity.class.getName();
-    protected TextView connectionStatusText;
-    protected TextView deviceText;
-    protected EditText ipAddressEdit;
-    protected Button openAppBtn;
-    protected Button saveIpButton;
-    private static BaseProduct mProduct;
-    private Handler mHandler;
-    private final AtomicBoolean isRegistrationInProgress = new AtomicBoolean(false);
-    public static final String FLAG_CONNECTION_CHANGE = "dji_sdk_connection_change";
+    private static final String TAG = ConnectionActivity.class.getName();
+
+    private TextView mTextConnectionStatus;
+    private TextView mTextProduct;
+    private TextView mVersionTv;
+    private Button mBtnOpen;
+    private EditText mEditIPAddress;
+    private Button mBtnSave;
+    private static final String[] REQUIRED_PERMISSION_LIST = new String[]{
+            Manifest.permission.VIBRATE,
+            Manifest.permission.INTERNET,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.WAKE_LOCK,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.CHANGE_WIFI_STATE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.READ_PHONE_STATE,
+    };
+    private List<String> missingPermission = new ArrayList<>();
+    private AtomicBoolean isRegistrationInProgress = new AtomicBoolean(false);
+    private static final int REQUEST_PERMISSION_CODE = 12345;
 
     @SuppressLint("UnspecifiedRegisterReceiverFlag")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            PermissionHandler.checkAndRequestPermissions(this);
-        }
-
+        checkAndRequestPermissions();
         setContentView(R.layout.activity_connection);
-
-        mHandler = new Handler(Looper.getMainLooper());
-
         initUI();
-
         // Register the broadcast receiver for receiving the device connection's changes.
         IntentFilter filter = new IntentFilter();
-        filter.addAction(FLAG_CONNECTION_CHANGE);
+        filter.addAction(ControllerApplication.FLAG_CONNECTION_CHANGE);
         registerReceiver(mReceiver, filter);
     }
 
-    private void initUI() {
-        connectionStatusText = findViewById(R.id.connection_status_text);
-        deviceText = findViewById(R.id.device_text);
-
-        openAppBtn = findViewById(R.id.open_app_btn);
-        openAppBtn.setOnClickListener(this);
-        openAppBtn.setEnabled(false);
-
-        saveIpButton = findViewById(R.id.save_ip_btn);
-        saveIpButton.setOnClickListener(this);
-
-        ipAddressEdit = findViewById(R.id.edit_ip_address);
-
-
-        // Load the saved text from SharedPreferences
-        String savedText = Preferences.loadText(this);
-        ipAddressEdit.setText(savedText);
+    /**
+     * Checks if there is any missing permissions, and
+     * requests runtime permission if needed.
+     */
+    private void checkAndRequestPermissions() {
+        // Check for permissions
+        for (String eachPermission : REQUIRED_PERMISSION_LIST) {
+            if (ContextCompat.checkSelfPermission(this, eachPermission) != PackageManager.PERMISSION_GRANTED) {
+                missingPermission.add(eachPermission);
+            }
+        }
+        // Request for missing permissions
+        if (!missingPermission.isEmpty() && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(this,
+                    missingPermission.toArray(new String[missingPermission.size()]),
+                    REQUEST_PERMISSION_CODE);
+        }
     }
 
+    /**
+     * Result of runtime permission request
+     */
     @Override
-    public void onClick(View view) {
-        int id = view.getId();
-        if (id == R.id.open_app_btn) {
-            Log.d("ButtonClick", "Open App button clicked");
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // Check for granted permission and remove from missing list
+        if (requestCode == REQUEST_PERMISSION_CODE) {
+            for (int i = grantResults.length - 1; i >= 0; i--) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    missingPermission.remove(permissions[i]);
+                }
+            }
         }
-        else if (id == R.id.save_ip_btn) {
-            saveText();
+        // If there is enough permission, we will start the registration
+        if (missingPermission.isEmpty()) {
+            startSDKRegistration();
         } else {
-            Log.d("ButtonClick", "Unknown button clicked");
+            showToast("Missing permissions!!!");
+        }
+    }
+
+    private void startSDKRegistration() {
+        if (isRegistrationInProgress.compareAndSet(false, true)) {
+            AsyncTask.execute(new Runnable() {
+                @Override
+                public void run() {
+                    showToast( "registering, pls wait...");
+                    DJISDKManager.getInstance().registerApp(getApplicationContext(), new DJISDKManager.SDKManagerCallback() {
+                        @Override
+                        public void onRegister(DJIError djiError) {
+                            if (djiError == DJISDKError.REGISTRATION_SUCCESS) {
+                                DJILog.e("App registration", DJISDKError.REGISTRATION_SUCCESS.getDescription());
+                                DJISDKManager.getInstance().startConnectionToProduct();
+                                showToast("Register Success");
+                            } else {
+                                showToast( "Register sdk fails, check network is available");
+                            }
+                            Log.v(TAG, djiError.getDescription());
+                        }
+
+                        @Override
+                        public void onProductDisconnect() {
+                            Log.d(TAG, "onProductDisconnect");
+                            showToast("Product Disconnected");
+
+                        }
+                        @Override
+                        public void onProductConnect(BaseProduct baseProduct) {
+                            Log.d(TAG, String.format("onProductConnect newProduct:%s", baseProduct));
+                            showToast("Product Connected");
+
+                        }
+
+                        @Override
+                        public void onProductChanged(BaseProduct baseProduct) {
+
+                        }
+
+                        @Override
+                        public void onComponentChange(BaseProduct.ComponentKey componentKey, BaseComponent oldComponent,
+                                                      BaseComponent newComponent) {
+
+                            if (newComponent != null) {
+                                newComponent.setComponentListener(new BaseComponent.ComponentListener() {
+
+                                    @Override
+                                    public void onConnectivityChange(boolean isConnected) {
+                                        Log.d(TAG, "onComponentConnectivityChanged: " + isConnected);
+                                    }
+                                });
+                            }
+                            Log.d(TAG,
+                                    String.format("onComponentChange key:%s, oldComponent:%s, newComponent:%s",
+                                            componentKey,
+                                            oldComponent,
+                                            newComponent));
+
+                        }
+
+                        @Override
+                        public void onInitProcess(DJISDKInitEvent djisdkInitEvent, int i) {
+
+                        }
+
+                        @Override
+                        public void onDatabaseDownloadProgress(long l, long l1) {
+
+                        }
+                    });
+                }
+            });
         }
     }
 
@@ -109,11 +208,6 @@ public class ConnectionActivity extends AppCompatActivity implements View.OnClic
         super.onStop();
     }
 
-    public void onReturn(View view){
-        Log.e(TAG, "onReturn");
-        this.finish();
-    }
-
     @Override
     protected void onDestroy() {
         Log.e(TAG, "onDestroy");
@@ -121,62 +215,31 @@ public class ConnectionActivity extends AppCompatActivity implements View.OnClic
         super.onDestroy();
     }
 
+    private void initUI() {
+
+        mTextConnectionStatus = (TextView) findViewById(R.id.text_connection_status);
+        mTextProduct = (TextView) findViewById(R.id.text_product_info);
+
+        mEditIPAddress = findViewById(R.id.edit_ip_address);
+        // Load the saved text from SharedPreferences
+        String savedText = Preferences.loadText(this);
+        mEditIPAddress.setText(savedText);
+
+        mBtnOpen = (Button) findViewById(R.id.btn_open);
+        mBtnOpen.setOnClickListener(this);
+        mBtnOpen.setEnabled(false);
+
+        mBtnSave = findViewById(R.id.btn_save);
+        mBtnSave.setOnClickListener(this);
+
+        mVersionTv = (TextView) findViewById(R.id.textView2);
+        mVersionTv.setText(getResources().getString(R.string.sdk_version, DJISDKManager.getInstance().getRegistrationSDKVersion()));
+    }
+
     private void saveText() {
-        String textToSave = ipAddressEdit.getText().toString();
+        String textToSave = mEditIPAddress.getText().toString();
         Preferences.saveText(this, textToSave);
         showToast("IP Address Saved");
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        PermissionHandler.onRequestPermissionsResult(this, requestCode, permissions, grantResults);
-    }
-
-    void notifyStatusChange() {
-        mHandler.removeCallbacks(updateRunnable);
-        mHandler.postDelayed(updateRunnable, 500);
-    }
-
-    private final Runnable updateRunnable = new Runnable() {
-        @Override
-        public void run() {
-            Intent intent = new Intent(FLAG_CONNECTION_CHANGE);
-            sendBroadcast(intent);
-        }
-    };
-
-    void showToast(final String toastMsg) {
-        UIUtils.showToast(getApplicationContext(), toastMsg);
-    }
-
-    void startSDKRegistration() {
-        if (isRegistrationInProgress.compareAndSet(false, true)) {
-            SDKRegistration.start(this, new SDKRegistrationCallback() {
-                @Override
-                public void onRegister(DJIError djiError) {
-                    if (djiError == DJISDKError.REGISTRATION_SUCCESS) {
-                        showToast("Register Success");
-                        DJISDKManager.getInstance().startConnectionToProduct();
-                    } else {
-                        showToast("Register sdk fails, please check the bundle id and network connection!");
-                    }
-                    Log.v(TAG, djiError.getDescription());
-                }
-
-                @Override
-                public void onProductDisconnect() {
-                    showToast("Product Disconnected");
-                    notifyStatusChange();
-                }
-
-                @Override
-                public void onProductConnect(BaseProduct baseProduct) {
-                    showToast("Product Connected");
-                    notifyStatusChange();
-                }
-            });
-        }
     }
 
     protected BroadcastReceiver mReceiver = new BroadcastReceiver() {
@@ -188,33 +251,51 @@ public class ConnectionActivity extends AppCompatActivity implements View.OnClic
     };
 
     private void refreshSDKRelativeUI() {
-        BaseProduct mProduct = getProductInstance();
+        BaseProduct mProduct = ControllerApplication.getProductInstance();
 
         if (null != mProduct && mProduct.isConnected()) {
+
             Log.v(TAG, "refreshSDK: True");
-            openAppBtn.setEnabled(true);
+            mBtnOpen.setEnabled(true);
 
             String str = mProduct instanceof Aircraft ? "DJIAircraft" : "DJIHandHeld";
-            connectionStatusText.setText("Status: " + str + " connected");
+            mTextConnectionStatus.setText("Status: " + str + " connected");
 
             if (null != mProduct.getModel()) {
-                deviceText.setText("Device: " + mProduct.getModel().getDisplayName());
+                mTextProduct.setText("" + mProduct.getModel().getDisplayName());
             } else {
-                deviceText.setText("Device: N/A");
+                mTextProduct.setText(R.string.product_information);
             }
-        } else {
-            Log.v(TAG, "refreshSDK: False");
-            openAppBtn.setEnabled(false);
 
-            deviceText.setText("Device: N/A");
-            connectionStatusText.setText("Status: No Device Connected");
+        } else {
+
+            Log.v(TAG, "refreshSDK: False");
+            mBtnOpen.setEnabled(false);
+
+            mTextProduct.setText(R.string.product_information);
+            mTextConnectionStatus.setText(R.string.connection_loose);
         }
     }
 
-    public static synchronized BaseProduct getProductInstance() {
-        if (null == mProduct) {
-            mProduct = DJISDKManager.getInstance().getProduct();
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId() == R.id.btn_open) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
         }
-        return mProduct;
+        else if(v.getId() == R.id.btn_save) {
+            saveText();
+        }
+    }
+
+    private void showToast(final String toastMsg) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), toastMsg, Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
--- app/src/main/java/com/aau/herd/controller/MApplication.java
+++ app/src/main/java/com/aau/herd/controller/MApplication.java
@@ -7,10 +7,20 @@ import com.secneo.sdk.Helper;
 
 public class MApplication extends Application {
 
+    private ControllerApplication controllerApplication;
     @Override
     protected void attachBaseContext(Context paramContext) {
         super.attachBaseContext(paramContext);
         Helper.install(MApplication.this);
+        if (controllerApplication == null) {
+            controllerApplication = new ControllerApplication();
+            controllerApplication.setContext(this);
+        }
     }
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        controllerApplication.onCreate();
+    }
 }
